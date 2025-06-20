152725b3288b4b2f14d49eb2116c8f210ade2285
IsaiCL <isaicampechanoleon@gmail.com>
2025-06-19 21:20:57 -0600

    Pregunta 3 - Correccion

--- src/components/Pregunta3.jsx
+++ src/components/Pregunta3.jsx
@@ -4,9 +4,23 @@
 import { useEffect } from 'react';
 
 function ClickLogger() {
+    /*CODIGO DESECHADO
     useEffect(() => {
         window.addEventListener('click', () => console.log('Click!'));
+    }, []);*/
+
+    //CODIGO CORREGIDO
+    useEffect(() => {
+    const handleClick = () => console.log('Click!');
+    window.addEventListener('click', handleClick);
+
+    return () => {
+        window.removeEventListener('click', handleClick);
+        };
     }, []);
+
+    //EXPLICACION: Se agregó una función de limpieza en el useEffect para evitar que múltiples event listeners se acumulen con cada montaje del componente. Esto previene duplicaciones de logs, mejora el rendimiento y respeta el ciclo de vida esperado del componente. También se asignó el callback handleClick a una constante para poder removerlo correctamente con removeEventListener.
+
 
     return <p>Haz clic en cualquier parte</p>;
 }
