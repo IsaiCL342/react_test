ae990eb9081b17b826f44e07b16e1e2cac15d84b
IsaiCL <isaicampechanoleon@gmail.com>
2025-06-19 21:27:48 -0600

    Pregunta 4 - Correcion

--- src/components/Pregunta4.jsx
+++ src/components/Pregunta4.jsx
@@ -4,11 +4,21 @@
 import { useEffect } from 'react';
 
 function Saludo({ nombre }) {
+    /*CODIGO DESECHADO
     if (nombre) {
         useEffect(() => {
         console.log('Hola ' + nombre);
         }, []);
-    }
+    }*/
+
+    //CODIGO CORREGIDO
+    useEffect(() => {
+    if (nombre) {
+        console.log('Hola ' + nombre);
+        }
+    }, [nombre]);
+
+    //EXPLICACION: Reestructuré el uso de useEffect para que no sea condicional. El hook ahora se llama siempre —como requiere la arquitectura de React— y contiene una condición interna que evita ejecutar la lógica innecesariamente. Esto elimina errores de ejecución y asegura que el orden de los hooks se mantenga estable en todos los renders.
 
     return <h1>Bienvenido</h1>;
 }
